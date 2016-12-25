347d6659fabd3f5710158a1d818a6e4aaaafdb5a
andrey <andrey@aurora.local>
2016-12-25 23:31:55 +0300

    dedicated client for each thread + code cleanup

--- tutorial-clnt/src/main/java/odin/j2ee/clnt/RestClient.java
+++ tutorial-clnt/src/main/java/odin/j2ee/clnt/RestClient.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.stream.Stream;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -16,26 +17,23 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 
 public class RestClient {
-	private static final int CONNECTIONS_LIMIT = 32;
+	private static final int CONNECTIONS_LIMIT = 10;
 	private static final int ITERATIONS_COUNT = 16;
 	private static final int CLIENTS_COUNT = 4;
 	
 	public static void main(String[] args) throws Exception {
 		ResteasyClientBuilder clientBldr = (ResteasyClientBuilder) ClientBuilder.newBuilder();
 		clientBldr.connectionPoolSize(CONNECTIONS_LIMIT);
-		clientBldr.maxPooledPerRoute(16);
-		ResteasyClient client = (ResteasyClient) clientBldr.build();
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
 		
 		int reqCount = ITERATIONS_COUNT * CLIENTS_COUNT;
 		CountDownLatch done = new CountDownLatch(reqCount);
 		InvocationCallback<Response> reqCompletionCallback = new TaskExecutionRequestCallback(done);
 		Thread[] threads = new Thread[CLIENTS_COUNT];
+		ResteasyClient[] clients = new ResteasyClient[CLIENTS_COUNT];
 		for (int tid = 0;tid < CLIENTS_COUNT;++tid) {
+			ResteasyClient client = clients[tid] = (ResteasyClient) clientBldr.build();
 			threads[tid] = new Thread(() -> {
 				for (int i = 0;i < ITERATIONS_COUNT;++i) {
-					// reader.readLine();
 					executeTask(client, reqCompletionCallback);
 				}
 			});
@@ -53,12 +51,10 @@ public class RestClient {
 		long stop = System.currentTimeMillis();
 		
 		System.out.printf("%d requests completed in %d ms%n", reqCount, stop - start);
-		
-		// TODO close client instead
-		System.out.println("sending shutdown signal to async request execution service");
-		client.asyncInvocationExecutor().shutdown();
+		Stream.of(clients).forEach(Client::close);
 		
 		System.out.print("press ENTER to exit...");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
 		reader.readLine();
 	}
 	
