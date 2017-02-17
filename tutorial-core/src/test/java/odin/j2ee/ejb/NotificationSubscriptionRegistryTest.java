1d2d0e2cb04f04a0494c83b464d365e0cef6a1f3
andrey <andrey@aurora.local>
2017-02-18 00:56:34 +0300

    using ear for packaging

--- tutorial-core/src/test/java/odin/j2ee/ejb/NotificationSubscriptionRegistryTest.java
+++ tutorial-core/src/test/java/odin/j2ee/ejb/NotificationSubscriptionRegistryTest.java
@@ -8,12 +8,15 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import odin.j2ee.api.NotificationSubscriptionRegistry;
 import odin.j2ee.model.TaskExecution;
+import odin.j2ee.model.TaskSupport;
 import odin.j2ee.task.TaskDetector;
 
 @RunWith(Arquillian.class)
@@ -21,7 +24,7 @@ public class NotificationSubscriptionRegistryTest {
 	@EJB
 	private NotificationSubscriptionRegistry registry;
 	
-	@Deployment
+//	@Deployment
 	public static JavaArchive createEjbJar() {
 		JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "core.jar")
 				.addPackage(NotificationSubscriptionRegistry.class.getPackage())
@@ -35,15 +38,36 @@ public class NotificationSubscriptionRegistryTest {
 		return archive;
 	}
 	
-//	public void setup() throws Exception {
-//		System.out.println("performing wfly pre-test setup");
-//		CommandContext cmdCtx = CommandContextFactory.getInstance().newCommandContext();
-//		cmdCtx.connectController();
-//		cmdCtx.handle("jms-topic add --topic-address=notifications --entries=java:/jms/topic/notifications");
-//		cmdCtx.handle("jms-queue add --queue-address=notifications --entries=java:/jms/queue/notifications --durable=false");
-//		cmdCtx.handle("jms-queue add --queue-address=tasks --entries=java:/jms/queue/tasks --durable=false");
-//		cmdCtx.terminateSession();
-//	}
+	@Deployment
+	public static EnterpriseArchive creatTestEar() {
+		EnterpriseArchive archive = ShrinkWrap.create(EnterpriseArchive.class, "test.ear");
+		JavaArchive coreJar = null;
+		try {
+		coreJar = ShrinkWrap.create(JavaArchive.class, "core.jar")
+				.addPackage(Package.getPackage("odin.j2ee.api"))
+				.addPackage(Package.getPackage("odin.j2ee.ejb"))
+				.addPackage(TaskSupport.class.getPackage())
+				.addPackage(TaskDetector.class.getPackage())
+				.addAsManifestResource("META-INF/ejb-jar.xml", "ejb-jar.xml");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		System.out.println(coreJar.toString(true));
+		archive.addAsModule(coreJar);
+		archive.addAsLibraries(Maven.resolver()
+				.loadPomFromFile("pom.xml")
+				.resolve("org.apache.commons:commons-collections4")
+				.withoutTransitivity()
+				.asFile());
+		archive.addAsLibraries(Maven.resolver()
+				.loadPomFromFile("pom.xml")
+				.resolve("ch.qos.logback:logback-classic")
+				.withTransitivity().
+				asFile());
+		archive.addAsManifestResource("META-INF/jboss-deployment-structure.xml", "jboss-deployment-structure.xml");
+		System.out.println(archive.toString(true));
+		return archive;
+	}
 	
 	@Test
 	public void testSubscribe() throws Exception {
