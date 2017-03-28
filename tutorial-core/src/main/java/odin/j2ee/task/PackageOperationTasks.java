b1388757097a622c79d85fbec7516526530bc27d
andrey <andrey@aurora.local>
2017-03-28 21:35:25 +0300

    loaded modules scanner

--- tutorial-core/src/main/java/odin/j2ee/task/PackageOperationTasks.java
+++ tutorial-core/src/main/java/odin/j2ee/task/PackageOperationTasks.java
@@ -2,8 +2,11 @@ package odin.j2ee.task;
 
 import java.lang.invoke.MethodHandles;
 import java.lang.management.ManagementFactory;
+import java.net.URL;
 import java.rmi.registry.LocateRegistry;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -13,7 +16,11 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.management.JMException;
 import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
 import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
@@ -44,8 +51,51 @@ public class PackageOperationTasks {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		performAppXmlScan();
+		processRunningModules();
 	}
 	
+	private void processRunningModules() {
+		log.debug("scanning module loaders");
+		MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+		try {
+			Set<ObjectInstance> serviceModuleLoaders = mbeanServer.queryMBeans(new ObjectName("jboss.modules:type=ModuleLoader,name=ServiceModuleLoader*"), null);
+			for (ObjectInstance serviceModuleLoader : serviceModuleLoaders) {
+				log.debug("scanning module loader: {}", serviceModuleLoader.getObjectName());
+				String[] loadedModules = (String[])mbeanServer.invoke(serviceModuleLoader.getObjectName(), "queryLoadedModuleNames", null, null);
+				log.debug("loaded modules: {}", Arrays.toString(loadedModules));
+			}
+		} catch (JMException jmxEx) {
+			log.error("failed to get list of loaded modules: ", jmxEx);
+		}
+	}
+
+	private void performAppXmlScan() {
+		log.debug("performing application.xml scanning");
+		
+		ClassLoader thisClassLoader = this.getClass().getClassLoader();
+		log.debug("this class loader: {}", thisClassLoader == null ? "null" : thisClassLoader.toString());
+		
+		ClassLoader thisClassParentLoader = thisClassLoader.getParent();
+		log.debug("this class parent loader: {}", thisClassParentLoader == null ? "null" : thisClassParentLoader.toString());
+		
+		ClassLoader threadContextLoader = Thread.currentThread().getContextClassLoader();
+		log.debug("thread context loader: {}", threadContextLoader == null ? "null" : threadContextLoader.toString());
+		
+		ClassLoader threadContextParentLoader = threadContextLoader.getParent();
+		log.debug("thread context parent loader: {}", threadContextParentLoader == null ? "null" : threadContextParentLoader.toString());
+		
+		ClassLoader libLoader = Logger.class.getClassLoader();
+		log.debug("lib loader: {}", libLoader == null ? "null" : libLoader.toString());
+		
+		URL appXmlUrl = libLoader.getResource("META-INF/application.xml");
+		log.debug("application.xml url: {}", appXmlUrl == null ? "null" : appXmlUrl.toString());
+		
+		ClassLoader libParentLoader = libLoader.getParent();
+		log.debug("lib parent loader: {}", libParentLoader == null ? "null" : libParentLoader.toString());
+	}
+
 	@PreDestroy
 	private void onDestroy() {
 		LoggerContext logCtx = (LoggerContext)LoggerFactory.getILoggerFactory();
